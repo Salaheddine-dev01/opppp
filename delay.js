bbadedf8fbe8e4923edff87fde4395aebe65ba47
Salah's Kingdom <salaheddine.01dev@gmail.com>
2025-04-29 23:39:28 +0100

    Add files via upload

---
+++ delay.js
@@ -0,0 +1,52 @@
+function verifierStock(produitId, quantite) {
+    return new Promise((resolve, reject) => {
+      console.log("Vérification du stock...");
+      setTimeout(() => {
+        if (produitId === 1 && quantite <= 5) {
+          resolve("Stock disponible.");
+        } else {
+          reject("Produit non disponible.");
+        }
+      }, 1000);
+    });
+  }
+  
+  function traiterPaiement(montant) {
+    return new Promise((resolve, reject) => {
+      console.log("Traitement du paiement...");
+      setTimeout(() => {
+        if (montant > 0) {
+          resolve("Paiement accepté.");
+        } else {
+          reject("Paiement refusé.");
+        }
+      }, 1000);
+    });
+  }
+  
+  function preparerCommande(commandId) {
+    return new Promise((resolve, reject) => {
+      console.log("Préparation de la commande...");
+      setTimeout(() => {
+        if (commandId) {
+          resolve("Commande préparée.");
+        } else {
+          reject("Erreur de préparation.");
+        }
+      }, 1000);
+    });
+  }
+  
+  function expedierCommande(commandId, adresse) {
+    return new Promise((resolve, reject) => {
+      console.log("Expédition de la commande...");
+      setTimeout(() => {
+        if (adresse !== "") {
+          resolve("Commande expédiée à " + adresse);
+        } else {
+          reject("Adresse invalide.");
+        }
+      }, 1000);
+    });
+  }
+  
